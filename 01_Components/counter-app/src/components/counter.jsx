e74898cd854a7e8c7195b8b5a9f96d86a89004bb
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-08-24 15:02:24 +0500

    increment op

--- 01_Components/counter-app/src/components/counter.jsx
+++ 01_Components/counter-app/src/components/counter.jsx
@@ -1,13 +1,42 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 
 class Counter extends Component {
     constructor(props) {
         super(props);
+        // Binding 'this' to countIncrement method
+        this.countIncrement = this.countIncrement.bind(this);
     }
-    state = {  }
+
+    state = { 
+        count: 1,
+        address: {
+            street: "Lane",
+            city: "youGanda",
+            country: "kenia",
+        }
+    };
+
+    countIncrement() {
+        // Increment the count state
+        this.setState(prevState =>({count:prevState.count + 2}));
+        this.setState(prevState => ({
+            count: prevState.count + 1
+        }));
+    }
+
+    formatCount() {
+        return this.state.count;
+    }
+
     render() { 
-        return <h1>Hello world</h1>;
+        return (
+            <div>
+                <h1>Hello world</h1>
+                <h2>Count: {this.formatCount()}</h2>
+                <button onClick={this.countIncrement}>Increment</button>    
+            </div>
+        );
     }
 }
- 
+
 export default Counter;
