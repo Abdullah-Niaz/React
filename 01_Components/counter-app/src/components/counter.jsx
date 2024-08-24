c25b0e95f956835c627806f96a81809d64c0295f
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-08-24 16:25:19 +0500

    Dynamic Bootstrap Casses

--- 01_Components/counter-app/src/components/counter.jsx
+++ 01_Components/counter-app/src/components/counter.jsx
@@ -1,42 +1,64 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    constructor(props) {
-        super(props);
-        // Binding 'this' to countIncrement method
-        this.countIncrement = this.countIncrement.bind(this);
-    }
+  state = {
+    count: 0,
+    address: {
+      street: "Lane",
+      city: "youGanda",
+      country: "kenia",
+    },
+  };
 
-    state = { 
-        count: 1,
-        address: {
-            street: "Lane",
-            city: "youGanda",
-            country: "kenia",
-        }
-    };
+  // Method to handle incrementing the count
+  countIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
 
-    countIncrement() {
-        // Increment the count state
-        this.setState(prevState =>({count:prevState.count + 2}));
-        this.setState(prevState => ({
-            count: prevState.count + 1
-        }));
-    }
+  formatCount() {
+    return this.state.count === 0 ? "Zero" : this.state.count;
+  }
 
-    formatCount() {
-        return this.state.count;
-    }
+  render() {
+        // Dynamic class assignment based on count value
+    let classes = this.getBadgeClasses();
+    return (
+      <div>
+        <span className={classes}>{this.formatCount()}</span>
+        <button
+          onClick={this.countIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  } 
 
-    render() { 
-        return (
-            <div>
-                <h1>Hello world</h1>
-                <h2>Count: {this.formatCount()}</h2>
-                <button onClick={this.countIncrement}>Increment</button>    
-            </div>
-        );
+
+    getBadgeClasses() {
+        let classes = "badge m-2 ";
+        classes += this.state.count === 0 ? "text-bg-warning" : "text-bg-primary";
+        return classes;
     }
 }
 
 export default Counter;
+
+
+// countIncrement() {
+//     // Increment the count state
+//     this.setState(prevState =>({count:prevState.count + 2}));
+//     this.setState(prevState => ({
+//         count: prevState.count + 1
+//     }));
+// }
+{
+  /* <h1>Hello world</h1> */
+}
+{
+  /* <h2>Count: {this.formatCount()}</h2> */
+}
+{
+  /* <button onClick={this.countIncrement}>Increment</button>     */
+}
