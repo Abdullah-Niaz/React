7179eed4031207b2f806db6b9876f196c7b50906
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-08-26 23:33:44 +0500

    FakeMovieGenerator

---
+++ 01_Components/vidly/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <i class="fas fa-h1">Hello World!</i>
+    </div>
+
+  );
+}
+
+export default App;
