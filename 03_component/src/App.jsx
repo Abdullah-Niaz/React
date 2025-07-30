0882f9b619b10ea086e9172685b6da2861763e96
Abdullah-Niaz <abdullahniaz423@gmail.com>
2025-07-30 16:14:01 +0500

    conditional rendering

--- 03_component/src/App.jsx
+++ 03_component/src/App.jsx
@@ -8,12 +8,12 @@ function App() {
 
   return (
     <>
-    <WellcomeComponent src="1.jpg" name="Gangland" rating="3.5"/>
-    <WellcomeComponent src="1.jpg" name="Gangland 1"rating="3.6"/>
-    <WellcomeComponent src="1.jpg" name="Gangland 2" rating="3.7"/>
-    <WellcomeComponent src="1.jpg" name="Gangland 3" rating="3.8"/>
-    <WellcomeComponent src="1.jpg" name="Gangland 4" rating="3.9"/>
-    <WellcomeComponent src="1.jpg" name="Gangland 5" rating="4"/>
+    <WellcomeComponent src="1.jpg" name="Gangland" rating="3.5" age="19"/>
+    <WellcomeComponent src="1.jpg" name="Gangland 1"rating="3.6" age="15"/>
+    <WellcomeComponent src="1.jpg" name="Gangland 2" rating="3.7" age="13"/>
+    <WellcomeComponent src="1.jpg" name="Gangland 3" rating="3.8" age="22"/>
+    <WellcomeComponent src="1.jpg" name="Gangland 4" rating="3.9" age="16"/>
+    <WellcomeComponent src="1.jpg" name="Gangland 5" rating="4" age="30"/>
     </>
   )
 }
@@ -28,7 +28,7 @@ const WellcomeComponent = (props)=>{
           <h1>{props.name}</h1>
           <h3>Rating: {props.rating}</h3>
           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque aut officiis facilis! Quasi voluptatibus ipsa consequatur alias sint a deleniti, nam eius exercitationem consequuntur quidem similique eos sunt quibusdam optio!</p>
-
+          <button>{props.age > 18 ? "Watch Now" : "Not Avaiable"}</button> You're: <span style={{color: "red", fontStyle:"bold"}} >{props.age}+</span>
         </div>
       </div>
   )
