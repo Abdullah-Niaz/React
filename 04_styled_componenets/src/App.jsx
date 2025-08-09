aa439580b912682ac8e7107d6b5a23250654eade
Abdullah-Niaz <abdullahniaz423@gmail.com>
2025-08-09 23:23:12 +0500

    styled components

--- 04_styled_componenets/src/App.jsx
+++ 04_styled_componenets/src/App.jsx
@@ -1,33 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import styled from 'styled-components'
+
+const StyledButton = styled.button`
+  background-color: #3498db;
+  color: white;
+  font-size: 1rem;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover{
+  background-color: #2980b9;
+  }
+`
 
 function App() {
   const [count, setCount] = useState(0)
+  const counter = ()=>{
 
+    setCount(preCount => preCount + 1)
+    setCount(preCount => preCount + 1)
+    setCount(preCount => preCount + 1)
+    setCount(preCount => preCount + 1)
+    setCount(preCount => preCount + 1)
+    setCount(preCount => preCount + 1)
+    setCount(preCount => preCount + 1)
+    setCount(preCount => preCount + 1)
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+     
+     
+        <StyledButton onClick={counter}>
           count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        </StyledButton>
+        
+
     </>
   )
 }
---
+++ 04_styled_componenets/src/Components/Dynamic/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+const DynamicButton = styled.button`
+    
+`
+
+export default function 
+() {
+  return
+  
+  (
+    <div>
+
+    </div>
+  )
+}
