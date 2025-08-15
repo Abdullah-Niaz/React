b0ee991cc978636f8bae00e22168294dd8844287
Abdullah-Niaz <abdullahniaz423@gmail.com>
2025-08-15 14:56:08 +0500

    state

---
+++ 08_states/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(10)
+  const handleButtonClickIncrement = () => {
+      setCount(count => count+1)
+  }
+
+  const handleButtonClickDecrement = () => {
+    setCount(count => count -1)
+  }
+  return (
+    <>
+     <h1>{count}</h1>
+     <button onClick={handleButtonClickIncrement}>Increment</button>
+     <button onClick={handleButtonClickDecrement}>Decrement</button>
+    </>
+  )
+}
+
+export default App
