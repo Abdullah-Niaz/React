0473c4d77474b4640875cbd1888dc546e081cca1
Abdullah-Niaz <abdullahniaz423@gmail.com>
2025-08-17 12:53:25 +0500

    states

--- 08_states/src/App.jsx
+++ 08_states/src/App.jsx
@@ -5,20 +5,36 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(10)
-  const handleButtonClickIncrement = () => {
-      setCount(count => count+1)
+  const increase = () => {
+      setCount(count => count+1);
   }
 
-  const handleButtonClickDecrement = () => {
-    setCount(count => count -1)
+  const decrease = () => {
+    setCount(count => count -1);
+  }
+  const reset  = ()=> {
+    setCount(0);
   }
   return (
-    <>
-     <h1>{count}</h1>
-     <button onClick={handleButtonClickIncrement}>Increment</button>
-     <button onClick={handleButtonClickDecrement}>Decrement</button>
-    </>
+   <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>React State Counter</h1>
+      <h2 style={{ color: count > 0 ? "blue" : "red" }}>
+        Count: {count}
+      </h2>
+      <button onClick={increase} style={btnStyle}>Increase</button>
+      <button onClick={decrease} style={btnStyle}>Decrease</button>
+      <button onClick={reset} style={btnStyle}>Reset</button>
+    </div>
   )
 }
 
 export default App
+
+
+const btnStyle = {
+  margin: "5px",
+  padding: "10px 20px",
+  fontSize: "16px",
+  borderRadius: "8px",
+  cursor: "pointer"
+};
