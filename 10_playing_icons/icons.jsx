9af66e38021a2f6080f629641abb8b756cc4e67a
Abdullah-Niaz <abdullahniaz423@gmail.com>
2025-08-21 15:23:14 +0500

    icons

--- 10_playing_icons/icons.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function icons() {
-  return (
-    <div>icons</div>
-  )
-}
---
+++ 10_playing_icons/src/ReactIcons.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AiFillApple } from "react-icons/ai";
+
+export default function ReactIcons() {
+  return (
+    <div>
+      <AiFillApple />
+    </div>
+  )
+}
