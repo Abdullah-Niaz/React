cc08db0d5c0e6015c912f22d953922b8045fe382
Abdullah-Niaz <abdullahniaz423@gmail.com>
2025-08-25 16:23:41 +0500

    capstone project

--- 11_hooks/src/main.jsx
+++ 11_hooks/src/main.jsx
@@ -3,10 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Index from './Components/hooks/useState/Index.jsx'
+import ControlledComponent from './Components/hooks/useState/ControlledComponent.jsx'
+import UnControlledComponent from './Components/hooks/useState/UnControlledComponent.jsx'
+
+import Challenge from './Components/hooks/useState/challenge.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
+    {/* <Challenge/> */}
     <Index/>
+    {/* <ControlledComponent/>
+    <UnControlledComponent/> */}
   </StrictMode>,
 )
