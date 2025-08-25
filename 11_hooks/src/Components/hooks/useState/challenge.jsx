509a5b11d46fce0a1dd7af932cefc20fe3ca776a
Abdullah-Niaz <abdullahniaz423@gmail.com>
2025-08-25 16:21:09 +0500

    challenge

--- 11_hooks/src/Components/hooks/useState/challenge.jsx
+++ 11_hooks/src/Components/hooks/useState/challenge.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { useState } from 'react'
 
-export default function challenge() {
+export default function Challenge() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
   return (
    <div className='container state-container' style={{textAlign:"center"}}>
         <h1>useState Hook</h1>
         <p>{count}</p>
-        <input type="text" onClick={} />
-        <button onClick={()=>setCount(count+1)}>Increment</button>
+        <input type="number" value={step} onChange={(e) => setStep(Number(e.target.value))} />
+        <button onClick={()=>setCount(count + step)}>Increment</button>
     </div>
   )
 }
